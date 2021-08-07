a675ff2420dcb3ad8f232ec771d948e34476df4e
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-08-07 22:51:26 +0200

    fix: parsing of simple progress

--- src/parsers.ts
+++ src/parsers.ts
@@ -4,7 +4,7 @@ import { parseSize } from "./utils/parseSize";
 import { timerToSecs } from "./utils/timerToSecs";
 
 const progressRxp =
-  /frame=\s*(?<frame>[\d]+)\s+fps=\s*(?<fps>[\d.]+)\s+q=(?<q>[0-9.-]+)\s+(L?)\s*size=\s*(?<size>[0-9]+)(?<sizeunit>kB|mB|b)?\s*time=\s*(?<time>\d\d:\d\d:\d\d\.\d\d)\s*bitrate=\s*(?<bitrate>N\/A|[\d.]+)(?<bitrateunit>bits\/s|mbits\/s|kbits\/s)?.*(dup=(?<duplicates>\d+)\s*)?(drop=(?<dropped>\d+)\s*)?speed=\s*(?<speed>[\d.e+]+)x/;
+  /(?:frame=\s*(?<frame>[\d]+)\s+)?(?:fps=\s*(?<fps>[\d.]+)\s+)?(?:q=(?<q>[0-9.-]+)\s+(L?)\s*)?size=\s*(?<size>[0-9]+)(?<sizeunit>kB|mB|b)?\s*time=\s*(?<time>\d\d:\d\d:\d\d\.\d\d)\s*bitrate=\s*(?<bitrate>N\/A|[\d.]+)(?<bitrateunit>bits\/s|mbits\/s|kbits\/s)?.*(dup=(?<duplicates>\d+)\s*)?(drop=(?<dropped>\d+)\s*)?speed=\s*(?<speed>[\d.e+]+)x/;
 export function parseProgress(data: string): FFmpeggyProgress | undefined {
   const matches = progressRxp.exec(data);
   if (!matches || !matches.groups) {
@@ -25,15 +25,22 @@ export function parseProgress(data: string): FFmpeggyProgress | undefined {
   } = matches.groups;
 
   return {
-    frame: Number(frame),
-    fps: Number(fps),
-    q: Number(q),
-    size: size && sizeunit ? parseSize(Number(size) || 0, sizeunit) : 0,
-    time: timerToSecs(time),
-    bitrate: bitrate && bitrateunit ? parseBitrate(Number(bitrate), bitrateunit): 0,
-    duplicates: Number(duplicates || 0),
-    dropped: Number(dropped || 0),
-    speed: Number(speed),
+    frame: typeof frame !== "undefined" ? Number(frame) : undefined,
+    fps: typeof frame !== "undefined" ? Number(fps) : undefined,
+    q: typeof frame !== "undefined" ? Number(q) : undefined,
+    size:
+      typeof size !== "undefined" && sizeunit
+        ? parseSize(Number(size) || 0, sizeunit)
+        : 0,
+    time: time ? timerToSecs(time) : undefined,
+    bitrate:
+      typeof bitrate !== "undefined" && bitrateunit
+        ? parseBitrate(Number(bitrate), bitrateunit)
+        : 0,
+    duplicates:
+      typeof duplicates !== "undefined" ? Number(duplicates) : undefined,
+    dropped: typeof dropped !== "undefined" ? Number(dropped) : undefined,
+    speed: typeof speed !== "undefined" ? Number(speed) : undefined,
   };
 }
 
--- src/types/FFmpeggyProgress.ts
+++ src/types/FFmpeggyProgress.ts
@@ -1,11 +1,11 @@
 export interface FFmpeggyProgress {
-  frame: number;
-  fps: number;
-  q: number;
-  size: number;
-  time: number;
-  bitrate: number;
-  duplicates: number;
-  dropped: number;
-  speed: number;
+  frame?: number;
+  fps?: number;
+  q?: number;
+  size?: number;
+  time?: number;
+  bitrate?: number;
+  duplicates?: number;
+  dropped?: number;
+  speed?: number;
 }
