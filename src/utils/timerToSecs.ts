fc41b7c3ee4dee34138273852538aabe17e13b28
Marcus Ekwall <marcus.ekwall@gmail.com>
2021-08-07 22:47:05 +0200

    fix(timerToSecs): round time to two decimals

--- src/utils/timerToSecs.ts
+++ src/utils/timerToSecs.ts
@@ -4,5 +4,5 @@ export function timerToSecs(input: string): number {
   const hours = +timer[0];
   const mins = +timer[1];
   const secs = parseFloat(timer[2]);
-  return hours * 3600 + mins * 60 + secs;
+  return Math.round((hours * 3600 + mins * 60 + secs) * 100) / 100;
 }
