752d1359e2b6940fa5bdbb1c58834dc514f5d368
mekwall <marcus.ekwall@gmail.com>
2021-08-13 14:35:35 +0200

    test: fix esm mport test

--- src/__tests__/importESModule.test.js
+++ src/__tests__/importESModule.test.js
@@ -1,9 +1,13 @@
+import { EventEmitter } from "stream";
+
 describe("import ES modules", () => {
   it("should be able to import", async () => {
     if (process.version.startsWith("v12")) {
       // Next line blows up on v12 so let's skip this for now
       return;
     }
+    // Ensure __signal_exit_emitter__ is defined or jest-runtime blows up
+    process.__signal_exit_emitter__ = new EventEmitter();
     const module = await import("../../es/index.mjs");
     expect(module.FFmpeggy).toBeDefined();
     const ffmpeggy = new module.FFmpeggy();
