ea94bb94439acb89a71f46f43f4d2117eb8cd61b
mekwall <marcus.ekwall@gmail.com>
2021-08-13 16:39:34 +0200

    chore: cleanup step 1/2

---
+++ cjs/types/FFmpegProgress.d.ts
@@ -0,0 +1,12 @@
+export interface FFmpegProgress {
+    frame: number;
+    fps: number;
+    q: number;
+    size: number;
+    time: number;
+    bitrate: number;
+    duplicates: number;
+    dropped: number;
+    speed: number;
+}
+//# sourceMappingURL=FFmpegProgress.d.ts.map
