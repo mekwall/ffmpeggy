193a1344352737cbd6aa1fc25cf328c94c7e676f
mekwall <marcus.ekwall@gmail.com>
2024-12-29 12:58:45 +0100

    chore(package): use separate release config

---
+++ release.config.js
@@ -0,0 +1,27 @@
+const isDryRun = process.env.DRY_RUN === "true";
+
+module.exports = {
+  branches: ["main"],
+  dryRun: isDryRun,
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    // Conditionally include plugins based on the dry-run flag
+    !isDryRun && "@semantic-release/npm",
+    !isDryRun && [
+      "@semantic-release/github",
+      {
+        assets: ["*.tgz"],
+      },
+    ],
+  ].filter(Boolean),
+};
