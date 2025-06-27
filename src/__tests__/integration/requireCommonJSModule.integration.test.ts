2fd8c9240d4c93172bf6aca85a9e9d635319791d
Marcus Ekwall <marcus.ekwall@gmail.com>
2025-06-27 02:28:12 +0200

    test(lint): add eslint disable comment for unicorn/prefer-module in CommonJS test

--- src/__tests__/integration/requireCommonJSModule.integration.test.ts
+++ src/__tests__/integration/requireCommonJSModule.integration.test.ts
@@ -2,7 +2,11 @@ import { describe, it, expect } from "vitest";
 
 describe("CommonJS build", () => {
   it("can be required and used", () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    // We can safely ignore the no-require-imports rule here because we are
+    // testing the CommonJS build of the library.
+    // We can also ignore the prefer-module rule because we are testing the
+    // CommonJS build of the library.
+    // eslint-disable-next-line @typescript-eslint/no-require-imports, unicorn/prefer-module
     const { FFmpeggy } = require("../../../dist/index.cjs");
     expect(FFmpeggy).toBeDefined();
     const ffmpeggy = new FFmpeggy();
